17c9b3b4021920fe628ca8cf4d107bdc4c5ae38d
danielsoaresti <danielsoares.ti@gmail.com>
2016-06-07 10:24:21 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
 import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyC52BBxOxEbd88ekLpznL5--Ni1q_bD2lw';
 
-YTSearch({key: API_KEY, term: 'nba'}, function(data){
-   console.log(data);
-});
-
 class App extends Component {
+   constructor(props){
+      super(props);
+
+      this.state = { videos: [], selectedVideo: null };
+
+      this.videoSearch('nba');
+   }
+
+   videoSearch(term){
+      YTSearch({key: API_KEY, term: term}, (videos) => {
+         this.setState({ videos: videos, selectedVideo: videos[0] });
+      });
+   }
+
     render (){
+      const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
       return (
          <div>
-            <SearchBar />
+            <SearchBar onSearchTermChange={ videoSearch } />
+            <VideoDetail video={ this.state.selectedVideo } />
+            <VideoList onVideoSelect={ selectedVideo => this.setState({selectedVideo})} videos={ this.state.videos } />
         </div>);
      };
 }
